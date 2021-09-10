618759b55095dd2728e6bcd41bc21d9e0438704a
Andrew Brkich <47767559+ABrkich@users.noreply.github.com>
2021-09-10 12:02:30 -0500

    Add files via upload

--- XMLParse/src/PublicationParser.java
+++ XMLParse/src/PublicationParser.java
@@ -10,6 +10,7 @@ public class PublicationParser {
         try {
             // Get the DOM Builder Factory
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            factory.setIgnoringElementContentWhitespace(true);
             // Get the DOM Builder
             DocumentBuilder builder = factory.newDocumentBuilder();
             // Load and Parse the XML document
@@ -18,104 +19,104 @@ public class PublicationParser {
             // Iterating through the nodes and extracting the data
             NodeList publications = document.getElementsByTagName("inproceedings");
             int numItems = publications.getLength();
-//            NodeList nodeList;
-//            try {
-//                nodeList = document.getDocumentElement().getChildNodes();
-//            }
-//            catch(NullPointerException e) {
-//                throw new Exception("Access denied - no childnodes");
-//            }
             List<Publication> pubList = new ArrayList<Publication>();
             for (int i = 0; i < publications.getLength(); i++) {
                 Node node = publications.item(i);
-                //if (node instanceof Element) {
-                    // We have encountered a publication tag
-                //NamedNodeMap testingobj = node.getAttributes();
-                NodeList testing = node.getChildNodes();
-                int xyz = testing.getLength();
-                    Publication pub = new Publication();
-                    try {
-                        pub.title = node.getAttributes().getNamedItem("title").getNodeValue();
+                NodeList childNode = node.getChildNodes();
+                int numChildren = childNode.getLength();
+                Publication pub = new Publication();
+                for (int z = 0; z < numChildren; z++)
+                {
+                    Node child = childNode.item(z);
+                    //System.out.println((child));
+                    String x = child.getNodeName();
+                    //System.out.println(x);
+                    if (x == "title")
+                    {
+                        pub.title = child.getNodeValue();
+                        String y = pub.title;
                     }
-                    catch(NullPointerException e) {
-                        throw new Exception("No Title on Publication, crash at item " + i + "!");
-                    }
-                    NodeList childNodes = node.getChildNodes();
-                    for (int j = 0; j < childNodes.getLength(); j++) {
-                        Node cNode = childNodes.item(j);
+                }
+////////////////
+//                    try {
+//                        pub.title = node.getAttributes().getNamedItem("title").getNodeValue();
+//                    }
+//                    catch(NullPointerException e) {
+//                        throw new Exception("No Title on Publication, crash at item " + i + "!");
+//                    }
                         // Identifying the child tag of a publication encountered
-                        if (cNode instanceof Element) {
-                            String content = cNode.getLastChild().getTextContent().trim();
-                            switch (cNode.getNodeName()) {
-                                case "editor":
-                                    pub.editor = content;
-                                    break;
-                                case "author":
-                                    pub.author = content;
-                                    break;
-                                case "booktitle":
-                                    pub.booktitle = content;
-                                    break;
-                                case "pages":
-                                    pub.pages = content;
-                                    break;
-                                case "year":
-                                    pub.year = content;
-                                    break;
-                                case "address":
-                                    pub.address = content;
-                                    break;
-                                case "journal":
-                                    pub.journal = content;
-                                    break;
-                                case "volume":
-                                    pub.volume = content;
-                                    break;
-                                case "number":
-                                    pub.number = content;
-                                    break;
-                                case "month":
-                                    pub.month = content;
-                                    break;
-                                case "url":
-                                    pub.url = content;
-                                    break;
-                                case "ee":
-                                    pub.ee = content;
-                                    break;
-                                case "cdrom":
-                                    pub.cdrom = content;
-                                    break;
-                                case "cite":
-                                    pub.cite = content;
-                                    break;
-                                case "publisher":
-                                    pub.publisher = content;
-                                    break;
-                                case "note":
-                                    pub.note = content;
-                                    break;
-                                case "crossref":
-                                    pub.crossref = content;
-                                    break;
-                                case "isbn":
-                                    pub.isbn = content;
-                                    break;
-                                case "series":
-                                    pub.series = content;
-                                    break;
-                                case "school":
-                                    pub.school = content;
-                                    break;
-                                case "chapter":
-                                    pub.chapter = content;
-                                    break;
-                                case "publnr":
-                                    pub.publnr = content;
-                                    break;
-                            }
-                        }
-                    }
+//                        if (cNode instanceof Element) {
+//                            String content = cNode.getLastChild().getTextContent().trim();
+//                            switch (cNode.getNodeName()) {
+//                                case "editor":
+//                                    pub.editor = content;
+//                                    break;
+//                                case "author":
+//                                    pub.author = content;
+//                                    break;
+//                                case "booktitle":
+//                                    pub.booktitle = content;
+//                                    break;
+//                                case "pages":
+//                                    pub.pages = content;
+//                                    break;
+//                                case "year":
+//                                    pub.year = content;
+//                                    break;
+//                                case "address":
+//                                    pub.address = content;
+//                                    break;
+//                                case "journal":
+//                                    pub.journal = content;
+//                                    break;
+//                                case "volume":
+//                                    pub.volume = content;
+//                                    break;
+//                                case "number":
+//                                    pub.number = content;
+//                                    break;
+//                                case "month":
+//                                    pub.month = content;
+//                                    break;
+//                                case "url":
+//                                    pub.url = content;
+//                                    break;
+//                                case "ee":
+//                                    pub.ee = content;
+//                                    break;
+//                                case "cdrom":
+//                                    pub.cdrom = content;
+//                                    break;
+//                                case "cite":
+//                                    pub.cite = content;
+//                                    break;
+//                                case "publisher":
+//                                    pub.publisher = content;
+//                                    break;
+//                                case "note":
+//                                    pub.note = content;
+//                                    break;
+//                                case "crossref":
+//                                    pub.crossref = content;
+//                                    break;
+//                                case "isbn":
+//                                    pub.isbn = content;
+//                                    break;
+//                                case "series":
+//                                    pub.series = content;
+//                                    break;
+//                                case "school":
+//                                    pub.school = content;
+//                                    break;
+//                                case "chapter":
+//                                    pub.chapter = content;
+//                                    break;
+//                                case "publnr":
+//                                    pub.publnr = content;
+//                                    break;
+//                            }
+//                        }
+
                     pubList.add(pub);
                 //}
             }
