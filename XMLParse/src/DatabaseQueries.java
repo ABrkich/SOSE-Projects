c426007705a6b422af34aa2b7192f74303b35acc
chuckthetruck <charlessayre94@gmail.com>
2021-09-15 15:23:33 -0500

    Finished GUI for SQL portion

--- XMLParse/src/DatabaseQueries.java
+++ XMLParse/src/DatabaseQueries.java
@@ -1,153 +1,136 @@
+import javax.xml.crypto.Data;
 import java.sql.*;
 import java.util.HashMap;
 import java.util.List;
 
 public class DatabaseQueries {
 
-    public static void getCoauthors(String author, Connection conn){
-        try {
-            String select_sql = "Select * from authors where author_id in (Select distinct author_id from paperstoauthors where paper_id in (Select paper_id from paperstoauthors pa left join authors a on a.author_id = pa.author_id where a.author_name = '" + author + "')) AND author_name != '" + author + "';";
-            System.out.println(select_sql);
-            Statement state = conn.createStatement();
-            ResultSet rs = state.executeQuery(select_sql);
-            while (rs.next()) {
-                String coauthor = rs.getString("author_name");
-                System.out.println("Coauthor: " + coauthor);
-            }
-        }catch (SQLException e) {
-            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+    public static String getCoauthors(String author) throws SQLException {
+
+        DatabaseQueries dbq = new DatabaseQueries();
+
+        String outstring = "";
+
+        String select_sql = "Select * from authors where author_id in (Select distinct author_id from paperstoauthors where paper_id in (Select paper_id from paperstoauthors pa left join authors a on a.author_id = pa.author_id where a.author_name = '" + author + "')) AND author_name != '" + author + "';";
+        //.out.println(select_sql);
+
+        Statement state = dbq.conn.createStatement();
+        ResultSet rs = state.executeQuery(select_sql);
+        while (rs.next()) {
+            String coauthor = rs.getString("author_name");
+            outstring+=  ("Coauthor: " + coauthor +"\n");
         }
+
+        return outstring;
     }
 
-    public static void getPaperMetadata(String title, Connection conn){
-        try {
-            String select_sql = "select * from papers right join paperstoauthors on papers.paper_id = paperstoauthors.paper_id join authors on authors.author_id = paperstoauthors.author_id where title = \""+ title + "\";";
-            Statement state = conn.createStatement();
-            ResultSet rs = state.executeQuery(select_sql);
+    public static String getPaperMetadata(String title) throws SQLException {
+        DatabaseQueries dbq = new DatabaseQueries();
+        String outstring = "";
+        String select_sql = "select * from papers right join paperstoauthors on papers.paper_id = paperstoauthors.paper_id join authors on authors.author_id = paperstoauthors.author_id where title = \""+ title + "\";";
+        Statement state = dbq.conn.createStatement();
+        ResultSet rs = state.executeQuery(select_sql);
 
-            String pages = "";
-            String year = "";
-            String url = "";
-            String ee = "";
-            String journal = "";
-            String volume = "";
-            String number = "";
-            String authors = "";
+        String pages = "";
+        String year = "";
+        String url = "";
+        String ee = "";
+        String journal = "";
+        String conf = "";
+        String volume = "";
+        String number = "";
+        String authors = "";
 
-            while (rs.next()) {
-                title = rs.getString("title");
-                pages = rs.getString("pages");
-                year = rs.getString("year");
-                url = rs.getString("url");
-                ee = rs.getString("ee");
-                journal = rs.getString("journal");
-                volume = rs.getString("volume");
-                number = rs.getString("number");
-                authors += (rs.getString("author_name") + ", ");
-            }
-
-            System.out.println("Title: " + title);
-            System.out.println("Authors: " + authors.substring(0,authors.length()-1));
-            System.out.println("Journal: " + journal);
-            System.out.println("Year: " + year);
-            System.out.println("Volume: " + volume);
-            System.out.println("Number: " + number);
-            System.out.println("Pages: " + pages);
-            System.out.println("Url: " + url);
-            System.out.println("EEs: " + ee);
-
-        }catch (SQLException e) {
-            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+        while (rs.next()) {
+            title = rs.getString("title");
+            pages = rs.getString("pages");
+            year = rs.getString("year");
+            url = rs.getString("url");
+            ee = rs.getString("ee");
+            conf = rs.getString("book_title");
+            journal = rs.getString("journal");
+            volume = rs.getString("volume");
+            number = rs.getString("number");
+            authors += (rs.getString("author_name") + ", ");
         }
-    }
-    public static void getJournalMetadata(String name, String year, String volume,String number, Connection conn){
-        try {
-            String select_sql = "select * from papers where journal = '" + name + "' and year = " + year + " and volume = "+ volume +" and number =" + number +";";
-            Statement state = conn.createStatement();
-            ResultSet rs = state.executeQuery(select_sql);
-
-            while (rs.next()) {
-                System.out.println("_________________________________________________________\n");
-                String title = rs.getString("title");
-                getPaperMetadata(title,conn);
-            }
-
-        }catch (SQLException e) {
-            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
-        }
-    }
-
-    public static void getConfPaperMetadata(String title, Connection conn){
-        try {
-            String select_sql = "select * from papers right join paperstoauthors on papers.paper_id = paperstoauthors.paper_id join authors on authors.author_id = paperstoauthors.author_id where title = \""+ title + "\";";
-            Statement state = conn.createStatement();
-            ResultSet rs = state.executeQuery(select_sql);
-
-            String pages = "";
-            String year = "";
-            String url = "";
-            String ee = "";
-            String conf = "";
-            String authors = "";
-
-            while (rs.next()) {
-                title = rs.getString("title");
-                pages = rs.getString("pages");
-                year = rs.getString("year");
-                url = rs.getString("url");
-                ee = rs.getString("ee");
-                conf = rs.getString("book_title");
-                authors += (rs.getString("author_name") + ", ");
-            }
-
-            System.out.println("Title: " + title);
+        if(conf == null) {
+            outstring += ("Title: " + title + "\n");
             if(authors.length() > 0) {
-                System.out.println("Authors: " + authors.substring(0, authors.length() - 2));
+                outstring += ("Authors: " + authors.substring(0, authors.length() - 2) + "\n");
             }
             else{
-                System.out.println("Authors: " + authors);
+                outstring += ("Authors: " + authors + "\n");
             }
-            System.out.println("Conference: " + conf);
-            System.out.println("Year: " + year);
-            System.out.println("Pages: " + pages);
-            System.out.println("Url: " + url);
-            System.out.println("EEs: " + ee);
-
-        }catch (SQLException e) {
-            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+            outstring += ("Journal: " + journal + "\n");
+            outstring += ("Year: " + year + "\n");
+            outstring += ("Volume: " + volume + "\n");
+            outstring += ("Number: " + number + "\n");
+            outstring += ("Pages: " + pages + "\n");
+            outstring += ("Url: " + url + "\n");
+            outstring += ("EEs: " + ee + "\n");
         }
+
+        else if(journal == null){
+            outstring += ("Title: " + title + "\n");
+
+            if(authors.length() > 0) {
+                outstring += ("Authors: " + authors.substring(0, authors.length() - 2) + "\n");
+            }
+            else{
+                outstring += ("Authors: " + authors + "\n");
+            }
+            outstring += ("Conference: " + conf + "\n");
+            outstring += ("Year: " + year + "\n");
+            outstring += ("Pages: " + pages + "\n");
+            outstring += ("Url: " + url + "\n");
+            outstring += ("EEs: " + ee + "\n");
+
+        }
+
+        return outstring;
     }
 
-    public static void getConferenceMetadata(String conf, String year, Connection conn){
-        try {
-            String select_sql = "select * from papers where book_title = '" + conf + "' and year = " + year + ";";
-            Statement state = conn.createStatement();
-            ResultSet rs = state.executeQuery(select_sql);
 
-            while (rs.next()) {
-                System.out.println("_________________________________________________________\n");
-                String title = rs.getString("title");
-                getConfPaperMetadata(title,conn);
-            }
+    public static String getJournalMetadata(String name, String volume,String number) throws SQLException {
+        DatabaseQueries dbq = new DatabaseQueries();
+        String outstring = "";
+        String select_sql = "select * from papers where journal = '" + name + "' and  volume = "+ volume +" and number =" + number +";";
+        Statement state = dbq.conn.createStatement();
+        ResultSet rs = state.executeQuery(select_sql);
 
-        }catch (SQLException e) {
-            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+        while (rs.next()) {
+            outstring += "_________________________________________________________\n";
+            String title = rs.getString("title");
+            outstring += getPaperMetadata(title);
         }
+
+        return outstring;
     }
-    public static void main(String[] args) {
+
+
+    public static String getConferenceMetadata(String conf, String year) throws SQLException {
+        DatabaseQueries dbq = new DatabaseQueries();
+        String outstring = "";
+        String select_sql = "select title from papers where book_title = '" + conf + "' and year = " + year + " and pages != 'null';";
+        Statement state = dbq.conn.createStatement();
+        ResultSet rs = state.executeQuery(select_sql);
+
+        while (rs.next()) {
+            outstring += "_________________________________________________________\n";
+            String title = rs.getString("title");
+            outstring += getPaperMetadata(title);
+        }
+
+        return outstring;
+        }
+        public static void main(String[] args) throws SQLException {
 
         // https://docs.oracle.com/javase/8/docs/api/java/sql/package-summary.html#package.description
         // auto java.sql.Driver discovery -- no longer need to load a java.sql.Driver class via Class.forName
 
         // register JDBC driver, optional since java 1.6
-        /*try {
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }*/
-
         // auto close connection
-        try (
+        /*try (
                 Connection conn = DriverManager.getConnection(
                         "jdbc:mysql://127.0.0.1:3306/CS7340_lab1", "root", "Chas3755")) {
 
@@ -157,18 +140,29 @@ public class DatabaseQueries {
                 System.out.println("Failed to make connection!");
             }
             Statement state = conn.createStatement();
-            getCoauthors("Athman Bouguettaya",conn);
-            getPaperMetadata("Long-Term QoS-Aware Cloud Service Composition Using Multivariate Time Series Analysis.", conn);
-            getJournalMetadata("IEEE Trans. Services Computing", "2016","9", "3", conn);
-            getConferenceMetadata("ICWS","2005", conn);
+            //getCoauthors("Athman Bouguettaya",conn);
+            //getPaperMetadata("Long-Term QoS-Aware Cloud Service Composition Using Multivariate Time Series Analysis.", conn);
+            //getJournalMetadata("IEEE Trans. Services Computing", "2016","9", "3", conn);
+            //getConferenceMetadata("ICWS","2005", conn);
 
 
         } catch (SQLException e) {
             System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
         } catch (Exception e) {
             e.printStackTrace();
-        }
+        }*/
 
+    }
+
+    static Connection conn;
+
+    static {
+        try {
+            conn = DriverManager.getConnection(
+                    "jdbc:mysql://127.0.0.1:3306/CS7340_lab1", "root", "Chas3755");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
 
--- XMLParse/src/DatabaseSubmission.java
+++ XMLParse/src/DatabaseSubmission.java
@@ -9,11 +9,6 @@ public class DatabaseSubmission {
         // auto java.sql.Driver discovery -- no longer need to load a java.sql.Driver class via Class.forName
 
         // register JDBC driver, optional since java 1.6
-        /*try {
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }*/
 
         // auto close connection
         try (
@@ -73,24 +68,6 @@ public class DatabaseSubmission {
             }
 
             System.out.println(DP.authorId);
-
-
-            /*for(Employee e: employees){
-                String sql = "insert into CS7340_lab1.employees values ('" + e.id + "','" + e.firstName + "','" + e.lastName + "','" + e.location + "')";
-                state.execute(sql);
-            }*/
-
-            /*String select_sql = "select * from employees";
-            ResultSet rs = state.executeQuery(select_sql);
-            while (rs.next()) {
-                String em_id = rs.getString("employee_id");
-                String em_fname = rs.getString("employee_fname");
-                String em_lname = rs.getString("employee_lname");
-                String em_location = rs.getString("employee_location");
-                System.out.println("ID: " + em_id + " First: " + em_fname + " Last: " + em_lname + " Location: " + em_location);
-            }*/
-
-
 
         } catch (SQLException e) {
             System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
